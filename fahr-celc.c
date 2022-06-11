286094ba3564424d63efa81ba118587c79502f9e
Martin Ashby <martin@ashbysoft.com>
2022-06-11 21:02:48 +0100

    more intro exercises

--- fahr-celc.c
+++ fahr-celc.c
@@ -3,9 +3,9 @@
 /* print farenheight-celcius 
 conversion table for 0, 20, ...300 */
 int main() {
-  for (float fahr=0.0;
-       fahr<=300.0;
-       fahr+=20.0) {
+  for (float fahr=300.0;
+       fahr>=0.0;
+       fahr-=20.0) {
     printf("%3.0f %6.1f\n",
       fahr,
       (5.0/9.0)*(fahr-32.0));
