dc405f573a2f44b8f3540a090f9c20fa89c56af0
Martin Ashby <martin@ashbysoft.com>
2022-06-14 19:29:27 +0100

    ex1-17

---
+++ ex1-17.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define minline 5
+/* reads lines from stdin 
+   outputs any lines over minlines
+   characters long */
+int main(void) {
+  int c, l = 0;
+  char buf[minline];
+  while ((c = getchar()) != EOF) {
+    if (c == '\n') {
+      if (l>minline)
+        putchar(c);
+      l = 0; 
+      for (int i=0;i<minline;i++)
+        buf[i]=0;
+    } else if (l<minline) {
+      buf[l]=c;
+      l++;
+    } else if (l==minline) {
+      for (int i=0;i<minline;i++)
+        putchar(buf[i]);
+      putchar (c);
+      l++;
+    } else {
+      putchar(c);
+      l++;
+    }
+  }
+  return 0;
+}
